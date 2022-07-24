9f15c6a3079d736845c3dd0cadf42fcfc2925537
Mauro <mauroromero0509@gmail.com>
2022-07-24 15:11:54 -0300

    primer commit

---
+++ precache-manifest.f3b528eb44a4fccf7fe5ccbdccb4934d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fb0cba3851a1e43aff29",
+    "url": "/static/css/main.c57ae545.chunk.css"
+  },
+  {
+    "revision": "fb0cba3851a1e43aff29",
+    "url": "/static/js/main.fb0cba38.chunk.js"
+  },
+  {
+    "revision": "58b692a9752b616b6892",
+    "url": "/static/css/1.6c086466.chunk.css"
+  },
+  {
+    "revision": "58b692a9752b616b6892",
+    "url": "/static/js/1.58b692a9.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f5228659c679cce6f211e81e940d87bc",
+    "url": "/index.html"
+  }
+];
